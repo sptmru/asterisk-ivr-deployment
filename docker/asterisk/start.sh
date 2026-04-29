f04cfbac294f23a831ed641469b766e9df20093b
Soslan Aldatov <soslanaldatov@gmail.com>
2026-04-29 18:07:42 +0400

    Refactor Asterisk Docker setup to unify directory creation in Dockerfile and start.sh

--- docker/asterisk/start.sh
+++ docker/asterisk/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p /var/spool/asterisk/voicemail /var/log/asterisk /var/run/asterisk
+mkdir -p \
+  /var/lib/asterisk \
+  /var/lib/asterisk/agi-bin \
+  /var/spool/asterisk/voicemail \
+  /var/log/asterisk \
+  /var/run/asterisk
 
 exec /usr/sbin/asterisk -f -U root -G root -vvv
